aaec852456a294ceb96b223ccec19b584e524d1f
ackhia <jffreelancing@gmail.com>
2023-03-20 10:06:39 +0700

    Returns 404 if not found

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ data = {}
 
 @app.api_route("/{path_name:path}", methods=["GET"])
 async def catch_all(request: Request, path_name: str):
-    print(data)
-    return Response(content = data[path_name])
+    if path_name in data:
+        return Response(content = data[path_name])
+    else:
+        return Response(status_code=404)
 
 @app.api_route("/{path_name:path}", methods=["POST"])
 async def catch_all(request: Request, path_name: str):
